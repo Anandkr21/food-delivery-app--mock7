c57b76d94b330202a2dcd347d330c0c1e96af977
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-06-01 14:57:21 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-    res.send('Welcome')
+    res.send('Welcome To Food Delivery app')
 })
 
 app.use('/api', userRouter)
